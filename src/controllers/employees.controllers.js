f47cc6b2a20afcf80c24e8c538058c040fbe619c
davidDev <juanda.vid1996@hotmail.com>
2023-04-07 17:54:41 -0500

    Se adiciona variables de entorno y se separan los endpoints de index.js a app.js

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,4 +1,4 @@
-import { pool } from "../db.js"
+import { pool } from '../db.js'
 
 
 // Obtener un solo dato
--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-import express from 'express';
-import indexRoutes from './routes/index.routes.js'
-import employeesRoutes from './routes/employees.routes.js'
+import app from './app.js'
+import { PORT } from './config.js'
 
-const app = express()
 
-app.use(express.json())
+app.listen(PORT)
+console.log('Server running on port',PORT)
 
-app.use('/v1',indexRoutes)
-app.use('/v1',employeesRoutes)
-
-// mensaje para cuando no se encuentre el endpoint
-app.use((req, res, next) => {
-    res.status(404).json({
-        message:'Not Found'
-    })
-})
-
-app.listen(3000)
-console.log('server running on port 3000')
